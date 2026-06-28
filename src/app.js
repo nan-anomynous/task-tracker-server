f312de93f29e31297ce1650f3799884f4057ccfe
nan-anomynous <nandinichaturvedi283@gmail.com>
2026-06-28 20:03:54 +0530

    first

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://task-tracker-client-l1fc.vercel.app"
+    "https://task-tracker-client-l1fc.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
