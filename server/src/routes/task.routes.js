2295940c9f9e5e97c8e81a26ddb5517f4146b670
nan-anomynous <nandinichaturvedi283@gmail.com>
2026-06-28 17:21:38 +0530

    Add files via upload

---
+++ server/src/routes/task.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    createTask,
+    getAllTasks,
+    getTaskById,
+    updateTask,
+    deleteTask
+} = require("../controllers/task.controller");
+
+router.post("/", createTask);
+
+router.get("/", getAllTasks);
+
+router.get("/:id", getTaskById);
+
+router.put("/:id", updateTask);
+
+router.delete("/:id", deleteTask);
+
+module.exports = router;
