00938ec2cccd845f0bde50265ed3b79c1f67d02f
nan-anomynous <nandinichaturvedi283@gmail.com>
2026-06-28 20:31:15 +0530

    first

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URL) {
-      console.error("MONGO_URI is not defined");
-      process.exit(1);
-    }
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected");
